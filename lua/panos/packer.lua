a04961d6ba15740ba26bf2b39510ace3846bdd3b
Panagiotis <panosakarajohn@hotmail.com>
2022-07-29 14:59:52 +0300

    Added treesitter

--- lua/panos/packer.lua
+++ lua/panos/packer.lua
@@ -8,12 +8,23 @@ return require('packer').startup(function()
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
   use 'folke/tokyonight.nvim'
+
+  --Telescope
   use {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.0',
     -- or                            , branch = '0.1.x',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
+
+  --Treesitter
+  use {
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+  }
+
+  use 'nvim-treesitter/playground'
+  use 'romgrk/nvim-treesitter-context'
 
 
 end)
--- lua/panos/remap.lua
+++ lua/panos/remap.lua
@@ -1,3 +1,4 @@
 local nnoremap = require("panos.keymap").nnoremap
 
 nnoremap("<leader>pv", "<cmd>Ex<CR>")
+nnoremap("<leader>q", vim.diagnostic.open_float)
