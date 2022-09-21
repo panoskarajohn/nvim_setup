2189565fd265da32debcbce0d19f73232a17c5e3
Panagiotis <panosakarajohn@hotmail.com>
2022-09-21 20:47:21 +0300

    Copy paster from primeagen looking forward to see what they do

--- lua/panos/remap.lua
+++ lua/panos/remap.lua
@@ -1,3 +1,47 @@
-local nnoremap = require("panos.keymap").nnoremap
+local Remap = require("panos.keymap")
+local nnoremap = Remap.nnoremap
+local vnoremap = Remap.vnoremap
+local inoremap = Remap.inoremap
+local xnoremap = Remap.xnoremap
+local nmap = Remap.nmap
 
+-- opens netrw
 nnoremap("<leader>pp", "<cmd>Ex<CR>")
+
+-- Similar to what Alt-Up || Alt-Down does on VS
+vnoremap("J", ":m '>+1<CR>gv=gv")
+vnoremap("K", ":m '<-2<CR>gv=gv")
+
+-- greatest remap ever
+xnoremap("<leader>p", "\"_dP")
+
+-- Something to look forward
+nnoremap("Y", "yg$")
+nnoremap("n", "nzzzv")
+nnoremap("N", "Nzzzv")
+nnoremap("J", "mzJ`z")
+nnoremap("<C-d>", "<C-d>zz")
+nnoremap("<C-u>", "<C-u>zz")
+
+-- next greatest remap ever : asbjornHaland
+nnoremap("<leader>y", "\"+y")
+vnoremap("<leader>y", "\"+y")
+nmap("<leader>Y", "\"+Y")
+
+nnoremap("<leader>d", "\"_d")
+vnoremap("<leader>d", "\"_d")
+
+vnoremap("<leader>d", "\"_d")
+
+-- Again something to look forward to 
+
+nnoremap("Q", "<nop>")
+nnoremap("<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
+nnoremap("<C-k>", "<cmd>cnext<CR>zz")
+nnoremap("<C-j>", "<cmd>cprev<CR>zz")
+nnoremap("<leader>k", "<cmd>lnext<CR>zz")
+nnoremap("<leader>j", "<cmd>lprev<CR>zz")
+
+nnoremap("<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
+nnoremap("<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
